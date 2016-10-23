18600cfcacb069f14b749646ababd857ad3509de
Scott <Scott@ScottsComp>
2016-10-23 13:57:25 -0500

    Master2.0 includes all updates as of 10/23/2016 located on Github, LifeStriders Repository, as well as edits made by my self including documentation in my volunteerForm.js, Tabs located in CounselingForm.html.

--- public_html/Scripts/counselForm.js
+++ public_html/Scripts/counselForm.js
@@ -1,46 +1,93 @@
 //JavaScript Document
 
 //Hide one div and show a div. tado
-//Should we have multipule JS documents or one for all forms.
 
 $(document).ready(function (e) {
-
+    var d = new Date();
+    var curr_date = d.getDate();
+    //Months are zero based
+    var curr_month = d.getMonth() + 1;
+    var curr_year = d.getFullYear();
+    $('html,body').scrollTop(0);
     $('#printPreviewConsultingFormOVER').hide();
+    
+    
 
+    var medId = 1;
+    $("#add-medication").click(function(){
+        medId++;
+        $("#meds").append('<div class="input-group medication-'+medId+'">'+
+            '\n\<label for="medication-'+medId+'-name">Medication</label>'+
+            '\n\<input type="text" class="medication-name" id="medication-'+medId+'-name">'+
+            '\n\<label for="medication-'+medId+'-prescriber">Prescribed By</label>'+
+            '\n\<input type="text" class="medication-prescriber" id="medication-'+medId+'-prescriber">'+
+            '\n\<label for="medication-'+medId+'-dosage">Dosage</label>'+
+            '\n\<input type="text" class="medication-dosage" id="medication-'+medId+'-dosage">'+
+            '\n\<label for="medication-'+medId+'-duration">Taken How Long?</label>'+
+            '\n\<input type="text" class="medication-duration" id="medication-'+medId+'-duration">'+
+            '\n\</div>')
+    });
 
-//Matt's way of creating print preview
     $('#printPreviewButton').click(function (e) {
-         //Contact
-        var todayDate = $('#todayDate').val();
-        var firstName = $('#firstName').val();
-        var lastName = $('#lastName').val();
-        var age = $('#age').val();
-        //Medications
-        var perscBy = $('#perscBy').val();
-        var medication = $('#medication').val();
-        var dosage = $('#dosage').val();
-        var howLongTaken = $('#howLongTaken').val();
-        //Current Medical Conditions
-        var allergie = $('#allergie').is( ':checked');
-        var asthma = $('#asthma').is( ':checked');
-        var arthritis = $('#arthritis').is( ':checked');
-        
-        //Print View
-        //Contact
-        $('#printPreviewTD').html("Today's Date: " + todayDate);
-        $('#printPreviewFN').html("FirstName: " + firstName);
-        $('#printPreviewLN').html("LastName: " + lastName);
-        $('#printPreviewAGE').html("Age: " + age);
-        //Medications
-        $('#printPreviewPB').html("Prescribed by: " + perscBy);
-        $('#printPreviewME').html("Medication: " + medication);
-        $('#printPreviewDO').html("Dosage: " + dosage);
-        $('#printPreviewHT').html("How long taken: " + howLongTaken);
-        //Current Medical Conditions
-        $('#printPreviewAL').html("Allergie: " + allergie);
-        $('#printPreviewAS').html("Asthma: " + asthma);
-        $('#printPreviewAR').html("Arthritis: " + arthritis);
+        var todaysdate = curr_month + "-" + curr_date + "-" + curr_year;
+        $('#printPreviewTD').html(todaysdate);
+        $('#printPreviewFN').html($('#firstName').val());
+        $('#printPreviewLN').html($('#lastName').val());
+        $('#printPreviewBD').html($('#birthDate').val());
+        $('#printPreviewSX').html($('#sex').val());
+        $('#printPreviewMS').html($('#marital').val());
 
+        $('#pAddress').html($('#cAddress').val());
+        $('#pCity').html($('#cCity').val());
+        $('#pState').html($('#cState').val());
+        $('#pZipCode').html($('#cZipCode').val());
+
+        $('#pHomePhone').html($('#homePhone').val());
+        $('#pMyEmail').html($('#myEmail').val());
+        $('#pWorkPhone').html($('#workPhone').val());
+        $('#pCellPhone').html($('#cellPhone').val());
+
+        $('#pEmployersName').html($('#employerName').val());
+        $('#pSocSecNum').html($('#socSecNum').val());
+
+        $('#pParentName').html($('#parentName').val());
+        $('#pHisHerDOB').html($('#hisherDOB').val());
+        $('#pHisHerSSN').html($('#hisherSSN').val());
+
+        $('#pEmerCon').html($('#emerCon').val());
+        $('#pEmerRel').html($('#emerRelation').val());
+        $('#pEmerPhone').html($('#emerPhone').val());
+
+        $('#pHearAbout').html($('#hearAbout').val());
+
+        $('#pAllegMed').html($('#allegMed').val());
+
+        $('#pCurrentCare').html($('#currentCare').val());
+        $('#pCurrentName').html($('#currentName').val());
+        $('#pCurrentDateSeen').html($('#currentDateSeen').val());
+        $('#pCurrentProblem').html($('#currentProblem').val());
+
+        $('#pMedDocName').html($('#medDoctorName').val());
+        $('#pMedDocPhone').html($('#medDocPhone').val());
+        $('#pMedDocDate').html($('#medDocDate').val());
+        
+        $('#pSigMedProb').html($('#sigMedProb').val());
+
+        $('#pPastSurg').html($('#pastSurg').val());
+        
+        $('#pPastAccid').html($('#pastAccid').val());
+        
+        $('#pRateHealth').html($('#rateHealth').val());
+        
+        $('#pReasonSeekTreat').html($('#reasonSeekTreat').val());
+        
+        $('#pExpectTreatment').html($('#expectTreatment').val());
+        
+        $('#pMentalHealthTreatment').html($('#mentalHealthTreatment').val());
+
+        //--------------------------------------------------------
+        
+        $('html,body').scrollTop(0);
         $('#printPreviewConsultingFormOVER').show();
         $('#formConsultingForm').hide();
     });
--- public_html/Scripts/volunteerForm.js
+++ public_html/Scripts/volunteerForm.js
@@ -3,11 +3,20 @@
 //10/13/2016
 
 $(document).ready(function (e) {
+     /*   
+      * UserException can be used to throw custom exceptions in Javascript,
+      * this helped myself with debuging the logic located within this Javascript file.
+     */
      function UserException(message) {
           this.message = message;
           this.name = "UserException";
-     } 
+     }
+     /*
+      * This will ask for Volunteer's age, and give the user either standard name/volunteer fields,
+      * or give them Parental Guardian fields.
+      */
      try {
+          //This will be taken from the given date in the above form.
           var age = prompt("How old are you", "00");
           if (age >= 18) {
                vfield = "<div class='form-group'><label for='volName'>Volunteer Name: </label>\n\<input type='text' class='form-control' id='volName'></div>\n\
@@ -27,6 +36,21 @@ $(document).ready(function (e) {
      $('#printPreviewVolunteerFormOVER').hide();
      $('#printPreviewButton').click(function () {
           try {
+               /*This is where the pages 1-4 print preview displays/id's would go Example would be 
+                * volInfoField = [
+                * {display: "Volunteer Name: " + $('#volName').val(), id: '#VN'},
+                * {display: "Volunteer Age: " + $('#volAge').val(), id: '#VA'}
+                * ];
+                * Nothing else is done here for additional fields, all gets taken care of by my functions,
+                * unless new data is entered into our map in which case, new functions are needed to 
+                * exctract data from the map.
+                */
+
+
+
+
+
+               //This will be taken from the given date in the above form.
                if (age >= 18) {
                     formfield = [
                          {display: "Volunteer Name: " + $('#volName').val(), id: '#VN'},
@@ -41,19 +65,29 @@ $(document).ready(function (e) {
                     throw new UserException("Invalid Age");
                }
                ;
+               /*
+                * Need to extract Display from map.
+                */
                function getDisplay(item, index) {
                     var getFullDisplay = [item.display];
                     return getFullDisplay;
                }
+               /*
+                * Need to extract id from map.
+                */
                function getId(item, index) {
                     var getFullIndex = [item.id];
                     return getFullIndex.toString();
                }
+               /*
+                * Loop over Array of formfields, grab the proper Display, id, and 
+                * output them into created list items in the printPreviewDisplay
+                * div in the HTML.
+                */
                for (i = 0, len = formfield.length; i < len; i++) {
                     $('#printPreviewDisplay').append("<li class='list-group-item'>" + formfield.map(getDisplay)[i] + "</li>");
                     $(formfield.map(getId)[i]).html(formfield.map(getDisplay)[i]);
                }
-               //$('#checklist').html("<p> Please keep this checklist, and bring it with you to your orientation. As you are instructed in certain fields, please ask your instructor/volunteer to initial the related checkbox. Please alert staff if you have been unable to receive training in all fields. Please hand in to staff when your checklist is complete. </p> <hr> <div class='box'></div><div class='field'>Tour of facility</div> <br> <div class='box'></div><div class='field'>Fire extinguishers, first aid kits, and emergency number locations</div> <br> <div class='box'></div><div class='field'>Where to go in case of tornado</div> <br> <div class='box'></div><div class='field'>Bathrooms and bubbler</div> <br> <div class='box'></div><div class='field'>Sign in and name tags</div> <br> <div class='box'></div><div class='field'>Tack room lay out and organization</div> <br> <div class='box'></div><div class='field'>Where treats are located and how many treats to feed</div> <br> <div class='box'></div><div class='field'>How clients should offer treats to horses</div> <br> <div class='box'></div><div class='field'>Quick Release ties and no horse left alone protocol</div> <br> <div class='box'></div><div class='field'>Stalls</div> <br> <div class='box'></div><div class='field'>Please don’t tie horses unless necessary, and never tie the horse with a rider on</div> <br> <div class='box'></div><div class='field'>Loading areas and protocol as well as gates</div> <br> <div class='box'></div><div class='field'>Safety stirrups</div> <br> <div class='box'></div><div class='field'>How to hold a lead rope</div> <br> <div class='box'></div><div class='field'>How to position correctly as a side walker</div> <br> <div class='box'></div><div class='field'>Different holds – on and off of the horse</div>");
           } catch (err) {
                alert(err.message);
           }
