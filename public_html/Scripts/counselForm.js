230a5d431afb37060bd3838c255f3e41cd53e032
mgparish <mgparish@gmail.com>
2016-10-26 18:16:48 -0500

    4.1 Got By OWN BRANCH

--- public_html/Scripts/counselForm.js
+++ public_html/Scripts/counselForm.js
@@ -1,7 +1,6 @@
 //JavaScript Document
 
 //Hide one div and show a div. tado
-//Should we have multipule JS documents or one for all forms.
 
 $(document).ready(function (e) {
 
@@ -10,36 +9,68 @@ $(document).ready(function (e) {
 
 
     $('#printPreviewButton').click(function (e) {
-         //Contact
-        var todayDate = $('#todayDate').val();
-        var firstName = $('#firstName').val();
-        var lastName = $('#lastName').val();
-        var age = $('#age').val();
-        //Medications
-        var perscBy = $('#perscBy').val();
-        var medication = $('#medication').val();
-        var dosage = $('#dosage').val();
-        var howLongTaken = $('#howLongTaken').val();
-        //Current Medical Conditions
-        var allergie = $('#allergie').is( ':checked');
-        var asthma = $('#asthma').is( ':checked');
-        var arthritis = $('#arthritis').is( ':checked');
-        
-        //Print View
-        //Contact
-        $('#printPreviewTD').html("Today's Date: " + todayDate);
-        $('#printPreviewFN').html("FirstName: " + firstName);
-        $('#printPreviewLN').html("LastName: " + lastName);
-        $('#printPreviewAGE').html("Age: " + age);
-        //Medications
-        $('#printPreviewPB').html("Prescribed by: " + perscBy);
-        $('#printPreviewME').html("Medication: " + medication);
-        $('#printPreviewDO').html("Dosage: " + dosage);
-        $('#printPreviewHT').html("How long taken: " + howLongTaken);
-        //Current Medical Conditions
-        $('#printPreviewAL').html("Allergie: " + allergie);
-        $('#printPreviewAS').html("Asthma: " + asthma);
-        $('#printPreviewAR').html("Arthritis: " + arthritis);
+
+        $('#printPreviewTD').html($('#todayDate').val());
+        $('#printPreviewFN').html($('#firstName').val());
+        $('#printPreviewLN').html($('#lastName').val());
+        $('#printPreviewBD').html($('#birthDate').val());
+        $('#printPreviewSX').html($('#sex').val());
+        $('#printPreviewMS').html($('#marital').val());
+
+        $('#pAddress').html($('#cAddress').val());
+        $('#pCity').html($('#cCity').val());
+        $('#pState').html($('#cState').val());
+        $('#pZipCode').html($('#cZipCode').val());
+
+        $('#pHomePhone').html($('#homePhone').val());
+        $('#pMyEmail').html($('#myEmail').val());
+        $('#pWorkPhone').html($('#workPhone').val());
+        $('#pCellPhone').html($('#cellPhone').val());
+
+        $('#pEmployersName').html($('#employerName').val());
+        $('#pSocSecNum').html($('#socSecNum').val());
+
+
+        if ($('#parentName').val().length === 0 || $('#hisherDOB').val().length === 0 || $('#hisherSSN').val().length === 0) {
+            $('.listRow5').hide();
+        } else {
+            $('#pParentName').html($('#parentName').val());
+            $('#pHisHerDOB').html($('#hisherDOB').val());
+            $('#pHisHerSSN').html($('#hisherSSN').val());
+        }
+
+        $('#pEmerCon').html($('#emerCon').val());
+        $('#pEmerRel').html($('#emerRelation').val());
+        $('#pEmerPhone').html($('#emerPhone').val());
+
+        $('#pHearAbout').html($('#hearAbout').val());
+
+        $('#pAllegMed').html($('#allegMed').val());
+
+        $('#pCurrentCare').html($('#currentCare').val());
+        $('#pCurrentName').html($('#currentName').val());
+        $('#pCurrentDateSeen').html($('#currentDateSeen').val());
+        $('#pCurrentProblem').html($('#currentProblem').val());
+
+        $('#pMedDocName').html($('#medDoctorName').val());
+        $('#pMedDocPhone').html($('#medDocPhone').val());
+        $('#pMedDocDate').html($('#medDocDate').val());
+
+        $('#pSigMedProb').html($('#sigMedProb').val());
+
+        $('#pPastSurg').html($('#pastSurg').val());
+
+        $('#pPastAccid').html($('#pastAccid').val());
+
+        $('#pRateHealth').html($('#rateHealth').val());
+
+        $('#pReasonSeekTreat').html($('#reasonSeekTreat').val());
+
+        $('#pExpectTreatment').html($('#expectTreatment').val());
+
+        $('#pMentalHealthTreatment').html($('#mentalHealthTreatment').val());
+
+        //--------------------------------------------------------
 
         $('#printPreviewConsultingFormOVER').show();
         $('#formConsultingForm').hide();
@@ -59,5 +90,3 @@ $(document).ready(function (e) {
 
 
 });
-
-
