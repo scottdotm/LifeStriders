c5d5299d52af1e81f50ff2a952217277d895176d
madgoat813 <mcfall86@gmail.com>
2016-11-02 10:51:27 -0500

    Added Liability release agreement to the print preview page

--- public_html/Scripts/ridingForm.js
+++ public_html/Scripts/ridingForm.js
@@ -1,10 +1,14 @@
 $(document).ready(function (e) {
-    
+    var d = new Date();
+    var curr_date = d.getDate();
+    //Months are zero based
+    var curr_month = d.getMonth() + 1;
+    var curr_year = d.getFullYear();
     $('html,body').scrollTop(0);
     $('#printPreviewRiderFormOVER').hide();
     
     $('#printPreviewButton').click(function (e) {
-        
+        var todaysdate = curr_month + "-" + curr_date + "-" + curr_year;
         if ($('#opt1').is(':checked')) {
             $('#pRiderType').text('Therapeutic Riding');
         }else if ($('#opt2').is(':checked')) {
@@ -26,6 +30,10 @@ $(document).ready(function (e) {
         $('#pPrimaryDob').html($('#primaryDob').val());
         $('#pAllergies').html($('#allergies').val());
         $('#pMedications').html($('#medications').val());
+        $('#participantSignedDate').html(todaysdate);
+        $('#guardianSignedDate').html(todaysdate);
+        $('#photoSignedDate').html(todaysdate);
+        
 
         //--------------------------------------------------------
         
